ee9c30d5d58a78526e09efc1531a11655107282f
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-11-16 13:15:49 +0100

    feat: tune further the CBFCBHybrid

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ ICM_all = load_ICM("in/data_ICM_title_abstract.csv")
 recommender = ItemKNNCBFCFSimilarityHybridRecommender(
     URM_train=URM_all,
     ICM_train=ICM_all,
-    topK_knncf=100,
+    topK_knncf=200,
     shrink_knncf=50,
-    topK_knncbf=500,
-    shrink_knncbf=10
+    similarity_knncf='cosine',
+    topK_knncbf=100,
+    shrink_knncbf=10,
+    similarity_knncbf='jaccard'
 )
 
-recommender.fit(topK=500, alpha=0.67)
+recommender.fit(topK=500, alpha=0.79)
 
 write_submission(recommender=recommender, target_users_path="in/data_target_users_test.csv",out_path='out/{}_submission.csv'.format(recommender.RECOMMENDER_NAME))
--- src/KNN/ItemKNNCBFCFSimilarityHybridRecommender.py
+++ src/KNN/ItemKNNCBFCFSimilarityHybridRecommender.py
@@ -16,7 +16,7 @@ class ItemKNNCBFCFSimilarityHybridRecommender(ItemKNNSimilarityHybridRecommender
     This recommender fuses a CBF approach and an item based CF approach
     """
 
-    RECOMMENDER_NAME = " ItemKNNCBFCFSimilarityHybridRecommender"
+    RECOMMENDER_NAME = "ItemKNNCBFCFSimilarityHybridRecommender"
 
     def __init__(
             self,
@@ -26,17 +26,18 @@ class ItemKNNCBFCFSimilarityHybridRecommender(ItemKNNSimilarityHybridRecommender
             shrink_knncf = 100,
             topK_knncbf = 50,
             shrink_knncbf = 100,
-            similarity='cosine',
+            similarity_knncbf='cosine',
+            similarity_knncf='cosine',
             verbose=True
     ):
 
         item_knncf_recommender = ItemKNNCFRecommender(URM_train=URM_train, verbose=verbose)
 
-        item_knncf_recommender.fit(topK=topK_knncf,shrink=shrink_knncf,similarity=similarity)
+        item_knncf_recommender.fit(topK=topK_knncf,shrink=shrink_knncf,similarity=similarity_knncf)
 
         item_knncbf_recommender = ItemKNNCBFRecommender(URM_train=URM_train, ICM_train=ICM_train, verbose=verbose)
 
-        item_knncbf_recommender.fit(topK=topK_knncbf,shrink=shrink_knncbf,similarity=similarity)
+        item_knncbf_recommender.fit(topK=topK_knncbf,shrink=shrink_knncbf,similarity=similarity_knncbf)
 
         super(ItemKNNCBFCFSimilarityHybridRecommender, self).__init__(
             URM_train,
