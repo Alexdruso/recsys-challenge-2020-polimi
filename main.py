13c4c388fad192b8411bac317a1310ca31f6b380
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-11-22 23:51:21 +0100

    feat: tune p3alpha + feature augmented rp3beta

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.Hybrid.SimilarityMergedHybridRecommender import SimilarityMergedHybridRecommender
 from src.GraphBased.P3alphaRecommender import P3alphaRecommender
 from src.GraphBased.RP3betaCBFRecommender import RP3betaCBFRecommender
+from src.Utils.ICM_preprocessing import *
 from src.Utils.load_URM import load_URM
 from src.Utils.load_ICM import load_ICM
 from src.Utils.write_submission import write_submission
@@ -8,18 +9,20 @@ from src.Utils.write_submission import write_submission
 URM_all = load_URM("in/data_train.csv")
 ICM_all = load_ICM("in/data_ICM_title_abstract.csv")
 
+ICM_all = augment(ICM=ICM_all, URM = URM_all)
+
 p3alpha_recommender = P3alphaRecommender(URM_train=URM_all)
-p3alpha_recommender.fit(topK=228,alpha=0.512,implicit=True)
+p3alpha_recommender.fit(topK=221,alpha=0.5017,implicit=True)
 
 rp3betaCBF_recommender = RP3betaCBFRecommender(URM_train=URM_all, ICM_train=ICM_all)
-rp3betaCBF_recommender.fit(topK=63, alpha=0.221, beta=0.341, implicit=False)
+rp3betaCBF_recommender.fit(topK=586, alpha=0.4228, beta=0.1918, implicit=False)
 
 recommender = SimilarityMergedHybridRecommender(
     URM_train=URM_all,
     CFRecommender=p3alpha_recommender,
     CBFRecommender=rp3betaCBF_recommender
 )
-recommender.fit(topK=406, alpha=0.68)
+recommender.fit(topK=355, alpha=0.2222)
 
 write_submission(recommender=recommender, target_users_path="in/data_target_users_test.csv",
                  out_path='out/{}_submission.csv'.format(recommender.RECOMMENDER_NAME))
