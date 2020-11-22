564d5f1ca3d98bd18e1643703139dbd96e758b6e
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-11-22 20:53:19 +0100

    feat: add ICM preprocessing

---
+++ src/Utils/ICM_preprocessing.py
@@ -0,0 +1,15 @@
+from scipy import sparse as sps
+import numpy as np
+
+def augment(ICM: sps.csr_matrix, URM : sps.csr_matrix):
+    return sps.hstack((URM.T, ICM), format='csr')
+
+def binarize(x):
+    if x != 0:
+        return 1
+    return x
+
+def binarize_ICM(ICM: sps.csr_matrix):
+    vbinarize = np.vectorize(binarize)
+
+    ICM.data = vbinarize(ICM.data)
