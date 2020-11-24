53724ea7320ce4a62815189c22a133321d12adea
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-11-24 12:33:03 +0100

    feat: tune P3alphaFeatureCombinedRP3beta

--- parameter_tuning/Hybrids/P3alphaFeatureCombinedRP3beta_tuning.py
+++ parameter_tuning/Hybrids/P3alphaFeatureCombinedRP3beta_tuning.py
@@ -1,20 +1,26 @@
-from scipy import sparse as sps
-
-from src.Base.Evaluation.Evaluator import EvaluatorHoldout
+from src.Base.Evaluation.K_Fold_Evaluator import K_Fold_Evaluator_MAP
+from src.Utils.ICM_preprocessing import *
 from src.Utils.load_ICM import load_ICM
 from src.Utils.load_URM import load_URM
-from src.Utils.ICM_preprocessing import *
 
 URM_all = load_URM("../../in/data_train.csv")
 ICM_all = load_ICM("../../in/data_ICM_title_abstract.csv")
 from src.Data_manager.split_functions.split_train_validation_random_holdout import \
     split_train_in_two_percentage_global_sample
 
-URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.80)
+URMs_train = []
+URMs_validation = []
 
-evaluator_validation = EvaluatorHoldout(URM_validation, cutoff_list=[10], verbose=False)
+for k in range(5):
+    URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.80)
+    URMs_train.append(URM_train)
+    URMs_validation.append(URM_validation)
 
-ICM_augmented = combine(ICM_all, URM_train)
+evaluator_validation = K_Fold_Evaluator_MAP(URMs_validation, cutoff_list=[10], verbose=False)
+
+ICMs_combined = []
+for URM in URMs_train:
+    ICMs_combined.append(combine(ICM=ICM_all, URM=URM))
 
 from src.Hybrid.SimilarityMergedHybridRecommender import SimilarityMergedHybridRecommender
 from src.GraphBased.P3alphaRecommender import P3alphaRecommender
@@ -22,21 +28,37 @@ from src.GraphBased.RP3betaCBFRecommender import RP3betaCBFRecommender
 
 from bayes_opt import BayesianOptimization
 
-p3alpha_recommender = P3alphaRecommender(URM_train=URM_train, verbose=False)
+p3alpha_recommenders = []
+rp3betaCBF_recommenders = []
 
-rp3betaCBF_recommender = RP3betaCBFRecommender(URM_train=URM_train, ICM_train=ICM_augmented, verbose=False)
+for index in range(len(URMs_train)):
+
+    p3alpha_recommenders.append(
+        P3alphaRecommender(
+            URM_train=URMs_train[index],
+            verbose=False
+        )
+    )
+
+    rp3betaCBF_recommenders.append(
+        RP3betaCBFRecommender(
+            URM_train=URMs_train[index],
+            ICM_train=ICMs_combined[index],
+            verbose=False
+        )
+    )
 
 tuning_params = {
     "cfTopK": (210, 230),
     "cfAlpha": (0.45, 0.52),
-    "cbfTopK": (600, 700),
+    "cbfTopK": (500, 600),
     "cbfAlpha": (0.4, 0.5),
     "cbfBeta": (0.2, 0.3),
     "hybridTopK": (10, 500),
     "hybridAlpha": (0.1, 0.9)
 }
 
-
+results = []
 def BO_func(
         cfTopK,
         cfAlpha,
@@ -46,33 +68,40 @@ def BO_func(
         hybridTopK,
         hybridAlpha
 ):
-    p3alpha_recommender.fit(
-        topK=int(cfTopK),
-        alpha=cfAlpha,
-        implicit=True
-    )
+    recommenders = []
 
-    rp3betaCBF_recommender.fit(
-        topK=int(cbfTopK),
-        alpha=cbfAlpha,
-        beta=cbfBeta,
-        implicit=False
-    )
+    for index in range(len(URMs_train)):
 
-    recommender = SimilarityMergedHybridRecommender(
-        URM_train=URM_train,
-        CFRecommender=p3alpha_recommender,
-        CBFRecommender=rp3betaCBF_recommender,
-        verbose=False
-    )
+        p3alpha_recommenders[index].fit(
+            topK=int(cfTopK),
+            alpha=cfAlpha,
+            implicit=True
+        )
 
-    recommender.fit(
-        topK=int(hybridTopK),
-        alpha=hybridAlpha
-    )
-    result_dict, _ = evaluator_validation.evaluateRecommender(recommender)
+        rp3betaCBF_recommenders[index].fit(
+            topK=int(cbfTopK),
+            alpha=cbfAlpha,
+            beta=cbfBeta,
+            implicit=False
+        )
 
-    return result_dict[10]["MAP"]
+        recommender = SimilarityMergedHybridRecommender(
+            URM_train=URMs_train[index],
+            CFRecommender=p3alpha_recommenders[index],
+            CBFRecommender=rp3betaCBF_recommenders[index],
+            verbose=False
+        )
+
+        recommender.fit(
+            topK=int(hybridTopK),
+            alpha=hybridAlpha
+        )
+
+        recommenders.append(recommender)
+
+    result = evaluator_validation.evaluateRecommender(recommenders)
+    results.append(result)
+    return sum(result) / len(result)
 
 
 optimizer = BayesianOptimization(
@@ -83,8 +112,8 @@ optimizer = BayesianOptimization(
 )
 
 optimizer.maximize(
-    init_points=150,
-    n_iter=60,
+    init_points=10,
+    n_iter=18,
 )
 
 hyperparameters = optimizer.max['params']
@@ -118,15 +147,46 @@ recommender.fit(
     alpha=hyperparameters['hybridAlpha']
 )
 
-optimizer.max['params']['cbfimplicit']=False
-optimizer.max['params']['cfimplicit']=True
-optimizer.max['params']['cbfTopK']=int(optimizer.max['params']['cbfTopK'])
-optimizer.max['params']['cfTopK']=int(optimizer.max['params']['cfTopK'])
-optimizer.max['params']['hybridTopK']=int(optimizer.max['params']['hybridTopK'])
+
 import json
 
 with open("logs/" + recommender.RECOMMENDER_NAME + "_logs.json", 'w') as json_file:
     json.dump(optimizer.max, json_file)
 
 
-#recommender.save_model(folder_path='../models/', file_name=recommender.RECOMMENDER_NAME)
+from src.Base.Evaluation.k_fold_significance_test import compute_k_fold_significance
+
+recommenders = []
+
+for index in range(len(URMs_train)):
+    p3alpha_recommenders[index].fit(
+        topK=int(optimizer.max["params"]["cfTopK"]),
+        alpha=optimizer.max["params"]["cfAlpha"],
+        implicit=True
+    )
+
+    rp3betaCBF_recommenders[index].fit(
+        topK=int(optimizer.max["params"]["cbfTopK"]),
+        alpha=optimizer.max["params"]["cbfAlpha"],
+        beta=optimizer.max["params"]["cbfBeta"],
+        implicit=False
+    )
+
+    recommender = SimilarityMergedHybridRecommender(
+        URM_train=URMs_train[index],
+        CFRecommender=p3alpha_recommenders[index],
+        CBFRecommender=rp3betaCBF_recommenders[index],
+        verbose=False
+    )
+
+    recommender.fit(
+        topK=int(optimizer.max["params"]["hybridTopK"]),
+        alpha=optimizer.max["params"]["hybridAlpha"]
+    )
+
+    recommenders.append(recommender)
+
+result = evaluator_validation.evaluateRecommender(recommenders)
+
+compute_k_fold_significance(result, *results)
+
