dac2702f25018dbcdddfd685e8b079bfc918a0ef
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-11-24 15:40:41 +0100

    fix: fix k fold evaluator

--- src/Base/Evaluation/K_Fold_Evaluator.py
+++ src/Base/Evaluation/K_Fold_Evaluator.py
@@ -11,21 +11,24 @@ from .Evaluator import EvaluatorHoldout
 class K_Fold_Evaluator_MAP(Evaluator):
 
     def __init__(self, URM_test_list: list, cutoff_list, min_ratings_per_user=1, exclude_seen=True,
-                 diversity_object=None, ignore_items=None, ignore_users=None, verbose=True):
+                 diversity_object=None, ignore_items=None, ignore_users_list = None, verbose=True):
 
         self.evaluator_list = []
 
-        for URM_test in URM_test_list:
+        if ignore_users_list == None:
+            ignore_users_list = [None]*len(URM_test_list)
+
+        for index in range(len(URM_test_list)):
 
             self.evaluator_list.append(
                 EvaluatorHoldout(
-                    URM_test_list=URM_test,
+                    URM_test_list=URM_test_list[index],
                     cutoff_list=cutoff_list,
                     min_ratings_per_user=min_ratings_per_user,
                     exclude_seen=exclude_seen,
                     diversity_object=diversity_object,
                     ignore_items=ignore_items,
-                    ignore_users=ignore_users,
+                    ignore_users=ignore_users_list[index],
                     verbose=verbose
                 )
             )
