f08c16f5bfb324e8c904d69b7b5f3086f1d80e82
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-11-27 15:06:13 +0100

    feat: tune KNN and derivatives

--- parameter_tuning/Hybrids/FeatureCombinedRP3betaItemKNN/FeatureCombinedRP3betaItemKNN_tuningBest25.py
+++ parameter_tuning/Hybrids/FeatureCombinedRP3betaItemKNN/FeatureCombinedRP3betaItemKNN_tuningBest25.py
@@ -10,13 +10,33 @@ from src.Data_manager.split_functions.split_train_validation_random_holdout impo
 
 URMs_train = []
 URMs_validation = []
+ignore_users_list = []
+
+import numpy as np
+
 
 for k in range(5):
     URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.80)
     URMs_train.append(URM_train)
     URMs_validation.append(URM_validation)
 
-evaluator_validation = K_Fold_Evaluator_MAP(URMs_validation, cutoff_list=[10], verbose=False)
+    profile_length = np.ediff1d(URM_train.indptr)
+    block_size = int(len(profile_length) * 0.25)
+
+    start_pos = 3 * block_size
+    end_pos = min(4 * block_size, len(profile_length))
+    sorted_users = np.argsort(profile_length)
+
+    users_in_group = sorted_users[start_pos:end_pos]
+
+    users_in_group_p_len = profile_length[users_in_group]
+    sorted_users = np.argsort(profile_length)
+
+    users_not_in_group_flag = np.isin(sorted_users, users_in_group, invert=True)
+    ignore_users_list.append(sorted_users[users_not_in_group_flag])
+
+evaluator_validation = K_Fold_Evaluator_MAP(URMs_validation, cutoff_list=[10], verbose=False,
+                                            ignore_users_list=ignore_users_list)
 
 ICMs_combined = []
 for URM in URMs_train:
@@ -54,7 +74,7 @@ tuning_params = {
     "rp3betaTopK": (500, 600),
     "rp3betaAlpha": (0.4, 0.5),
     "rp3betaBeta": (0.2, 0.3),
-    "hybridTopK": (10, 500),
+    "hybridTopK": (10, 1000),
     "hybridAlpha": (0.1, 0.9)
 }
 
